cc52b40e296a60e537a98dbab4c9a95be278277e
AnlesSai <Anastasia.lesn@gmail.com>
2023-12-18 00:11:19 +0300

    feat: part of tasks

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -211,8 +211,8 @@ function sumOfCodes(str) {
  *   startsWith('Hello World', 'World') => false
  *   startsWith('Hello World', 'Hello') => true
  */
-function startsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function startsWith(str, substr) {
+  return str.startsWith(substr);
 }
 
 /**
@@ -226,8 +226,8 @@ function startsWith(/* str, substr */) {
  *   endsWith('Hello World', 'World') => true
  *   endsWith('Hello World', 'Hello') => false
  */
-function endsWith(/* str, substr */) {
-  throw new Error('Not implemented');
+function endsWith(str, substr) {
+  return str.endsWith(substr);
 }
 
 /**
@@ -243,8 +243,10 @@ function endsWith(/* str, substr */) {
  *   formatTime(0, 45) => "00:45"
  *   formatTime(0, 0) => "00:00"
  */
-function formatTime(/* minutes, seconds */) {
-  throw new Error('Not implemented');
+function formatTime(minutes, seconds) {
+  const a = minutes.toString().padStart(2, '0');
+  const b = seconds.toString().padStart(2, '0');
+  return `${a}:${b}`;
 }
 
 /**
